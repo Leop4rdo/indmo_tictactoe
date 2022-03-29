bafc6c872a8759987465354d443d9abfd77e1f74
Leonardo Antunes <leonardoantunes1401@gmail.com>
2022-03-29 15:28:07 -0300

    Adicionando dialog de game over

--- app/src/main/java/br/senai/sp/jandira/tictactoe/GameActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/tictactoe/GameActivity.kt
@@ -1,17 +1,31 @@
 package br.senai.sp.jandira.tictactoe
 
+import android.app.Dialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Window
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import br.senai.sp.jandira.tictactoe.databinding.ActivityGameBinding
+import br.senai.sp.jandira.tictactoe.databinding.GameOverDialogBinding
+import kotlinx.android.synthetic.main.game_over_dialog.*
 
 class GameActivity : AppCompatActivity() {
     lateinit var binding: ActivityGameBinding
 
     val tokens = arrayOf(R.drawable.ic_token_x, R.drawable.ic_token_o)
 
-    /** estado atual dos tiles */
+    /**
+     * guarda o estado atual dos tiles do jogo
+     *
+     * 0 -> preenchido pelo jogador 1
+     * 1 -> preenchido pelo jogador 2
+     * 2 -> tile não preenchido ainda
+     */
     val gameState = IntArray(9) { 2 }
 
     var gameOver = false
@@ -24,56 +38,55 @@ class GameActivity : AppCompatActivity() {
     var activePlayer = 0
 
     /** responsavel por determinar quantas jogadas ja foram realizadas */
-    var turnCount = 0;
+    var turnCount = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGameBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        /** aviso de funcionalidade ainda em desenvolvimento */
-        val dialogWIP = Toast.makeText(this, R.string.dialog_wip, Toast.LENGTH_SHORT)
-
         /** array responsavel por armazenar todos os tiles do board */
         val gameTiles = arrayOf<ImageView>(binding.tile0, binding.tile1, binding.tile2,
                                             binding.tile3, binding.tile4, binding.tile5,
                                             binding.tile6, binding.tile7, binding.tile8)
 
-        // o bloco de codigo abaixo defini o ClickListener de cada tile para chamar a função onPLayerTap();
+        // o bloco de codigo abaixo define o ClickListener de cada tile para chamar a função makeMove(tile);
         gameTiles.forEach {
-            it.setOnClickListener { onPlayerTap(it as ImageView) }
+            it.setOnClickListener { makeMove(it as ImageView) }
         }
 
         binding.buttonVoltar.setOnClickListener { finish() }
-        binding.buttonHome.setOnClickListener { dialogWIP.show() }
+        binding.buttonHome.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+
+            startActivity(intent)
+        }
     }
 
     /**
      * função chamada quando o jogador realiza um movimento
      */
-    private fun onPlayerTap(tile: ImageView) {
+    private fun makeMove(tile: ImageView) {
+        // pegando o index do tile no array gameState[]
+        val tileIndex = tile.getTag().toString().toInt();
+
         // se o jogo ja terminou, impede a realização de novos movimentos
         if (gameOver) return
 
-        val tileId = tile.getTag().toString().toInt();
-
         // se o tile ja  estiver preenchido, impede a jogada e exibe uma mensagem
-        if (gameState[tileId] != 2) return Toast.makeText(this, R.string.dialog_invalid_move, Toast.LENGTH_SHORT).show()
+        if (gameState[tileIndex] != 2) return Toast.makeText(this, R.string.dialog_invalid_move, Toast.LENGTH_SHORT).show()
 
         // realizando a jogada
         tile.setImageResource(tokens[activePlayer]);
-        gameState[tileId] = activePlayer;
+        gameState[tileIndex] = activePlayer;
 
         turnCount++
 
-        gameOver = isGameOverByWin() || turnCount >= 9
-        Toast.makeText(this, "gameOver -> ${gameOver}", Toast.LENGTH_SHORT).show()
-
         // verifica se alguem ganhou
-        if (isGameOverByWin()) return Toast.makeText(this, "Game Over, player ${activePlayer} ganhou!", Toast.LENGTH_SHORT).show()
+        if (isGameOverByWin()) showGameOverDialog("Player ${activePlayer + 1} Ganhou!!!")
 
         // verifica se deu velha
-        if (turnCount >= 9) return Toast.makeText(this, "Game Over, ninguem ganhou!", Toast.LENGTH_SHORT).show()
+        if (turnCount >= 9) showGameOverDialog("Empate!!!")
 
         // alterando o jogador ativo
         updateActivePlayer()
@@ -127,6 +140,35 @@ class GameActivity : AppCompatActivity() {
     private fun isDiagonalWin() : Boolean {
         return (gameState[0] != 2 && gameState[0] == gameState[4] && gameState[4] == gameState[8]) ||
                 (gameState[2] != 2 && gameState[2] == gameState[4] && gameState[4] == gameState[6])
+
+    }
+
+    private fun showGameOverDialog(message: String) {
+        val dialog: Dialog = Dialog(this);
+        val dialogBinding: GameOverDialogBinding =  GameOverDialogBinding.inflate(layoutInflater)
+
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(dialogBinding.root);
+
+        dialogBinding.dialogTitle.text = message;
+
+        dialogBinding.dialogButtonHome.setOnClickListener {
+
+            val intent = Intent(this, MainActivity::class.java);
+
+            startActivity(intent);
+        }
+
+        dialogBinding.dialogButtonRestart.setOnClickListener {
+            restartGame()
+            dialog.dismiss()
+        }
+
+        dialog.show()
+    }
+
+    private fun restartGame() {
 
     }
 }
